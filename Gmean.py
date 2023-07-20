e3095e114693a1c5255bf241139eb3982edd334b
MihikaSingh18 <138354730+MihikaSingh18@users.noreply.github.com>
2023-07-20 21:45:26 +0530

    Add files via upload

---
+++ Gmean.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      singh
+#
+# Created:     20-07-2023
+# Copyright:   (c) singh 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+def calculateGmean(a , b):
+    mean = (a*b)/(a+b)
+    print(mean)
+a=5
+b=5
+calculateGmean(a ,b)
